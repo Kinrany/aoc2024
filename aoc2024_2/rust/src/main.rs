cf89cee8f54469701bf747318003c0777969455a
Ruslan Fadeev <github@kinrany.dev>
2024-12-01 18:05:50 +0400

    solve aoc2024_2 in rust

---
+++ aoc2024_2/rust/src/main.rs
@@ -0,0 +1,48 @@
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::Context;
+
+fn input(input: impl BufRead) -> anyhow::Result<Vec<(i32, i32)>> {
+    input
+        .lines()
+        .map(|result| {
+            let s = result.context("failed to read line")?;
+            let (a, b) = s.split_once("   ").context("invalid line")?;
+            Ok((a.parse()?, b.parse()?))
+        })
+        .collect()
+}
+
+fn solution(input: Vec<(i32, i32)>) -> i32 {
+    let (list1, list2): (Vec<_>, Vec<_>) = input.into_iter().unzip();
+    let counts = list2.into_iter().fold(HashMap::new(), |mut acc, x| {
+        *acc.entry(x).or_insert(0) += 1;
+        acc
+    });
+    list1
+        .into_iter()
+        .map(|x| x * counts.get(&x).unwrap_or(&0))
+        .sum()
+}
+
+fn main() -> anyhow::Result<()> {
+    let answer = solution(input(BufReader::new(std::io::stdin()))?);
+    println!("{answer}");
+    Ok(())
+}
+
+#[test]
+fn parse_input() {
+    let txt = include_bytes!("../../example.txt");
+    let input = input(txt.as_slice()).unwrap();
+    insta::assert_debug_snapshot!(input);
+}
+
+#[test]
+fn solve_example() {
+    let input = vec![(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)];
+    assert_eq!(solution(input), 31);
+}
